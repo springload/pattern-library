e95ae2031db5eddc59a3239d2adb8f9c8233370c
Josh Barr <josh@joshbarr.com>
2016-03-29 10:00:59 +1300

    modified componentys

--- patterns/components/radio_picker/radio_picker.py
+++ patterns/components/radio_picker/radio_picker.py
@@ -1,8 +1,6 @@
 import json
-from patterns.base_component import BaseComponent
+from patterns.components.base import BaseComponent
 
-with './config.json' as file:
-    DEFAULT = json.load(file)
 
 
 class RadioPicker(BaseComponent):
--- patterns/views.py
+++ patterns/views.py
@@ -32,5 +32,3 @@ def index(request):
             ]
         }
     })
-
-
