2ea15a0c2abb3df1e70eb00257f0192ac1c66e8b
Josh Barr <josh@joshbarr.com>
2016-03-30 17:43:23 +1300

    pythonism

--- patterns/templatetags/component_tags.py
+++ patterns/templatetags/component_tags.py
@@ -190,8 +190,13 @@ class PrettyPrintNode(Node):
 
     def render(self, context):
         html = BeautifulSoup(self.nodelist.render(context))
-        # html.html.unwrap()
-        # html.body.unwrap()
+
+        try:
+            html.html.unwrap()
+            html.body.unwrap()
+        except:
+            pass
+
         return html.prettify()
 
 
